2f6431898cb4b58b564dcd4fd7f57e880e6d1dab
Jose Ferreira <jeduardoferreira.03@gmail.com>
2026-03-04 14:42:19 -0300

    Updated desafio18

--- Mundo 4/desafio18.py
+++ Mundo 4/desafio18.py
@@ -5,21 +5,23 @@ from rich import print
 
 class Churrasco:
     
+    # Atributos de Classe
+    consumo_padrao = 0.4  # 400g por pessoa
+    valor_carne = 82.40  # R$82,40 por kg
+
     def __init__(self, titulo, quant):
         self.titulo = titulo
         self.quant = quant
     
     # Método para analisar a quantidade de carne necessária, valor total do churrasco e valor individual por pessoa
     def analisar(self):
-        consumo_padrao = 0.4  # 400g por pessoa
-        valor_carne = 82.40  # R$82,40 por kg
-        quant_recomend = consumo_padrao * self.quant
-        custo_total = valor_carne * quant_recomend
+        quant_recomend = Churrasco.consumo_padrao * self.quant
+        custo_total = Churrasco.valor_carne * quant_recomend
         custo_individual = custo_total / self.quant
 
         tbl_analise = Panel(f'''
         Analisando {self.titulo} com {self.quant} convidados
-        Cada participante comerá {consumo_padrao}Kg, Cada Kg custa R${valor_carne:.2f}
+        Cada participante comerá {Churrasco.consumo_padrao}Kg, Cada Kg custa R${Churrasco.valor_carne:.2f}
         Recomendo comprar {quant_recomend:.3f}Kg de carne
         Valor total: R${custo_total:.2f}
         Valor individual: R${custo_individual:.2f}
@@ -29,5 +31,5 @@ class Churrasco:
         
         print(tbl_analise)
 
-churras1 = Churrasco("Churras dos Brothers", 3)
+churras1 = Churrasco("Churras dos Brothers", 15)
 churras1.analisar()
